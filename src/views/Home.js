8ca5c9c492842533dc1cbb3c8c39ea0b3568dbc1
Eric Bishard <e.bishard@gmail.com>
2022-03-30 04:52:54 +0400

    initial start branch commit

---
+++ src/views/Home.js
@@ -0,0 +1,34 @@
+import { Link, HashRouter, Route, Routes } from 'react-router-dom'
+import GlobalStyles from '../theme/GlobalStyles'
+
+/* Imports */
+import Tickets from '../components/templates/Tickets'
+
+const Home = () => {
+  /* Top Level Code */
+  return (
+    <HashRouter>
+      <GlobalStyles />
+      <header>
+        {/* Header */}
+      </header>
+      <nav>
+        <ul>
+          <li><Link to={`/`}>Tickets</Link></li>
+        </ul>
+      </nav>
+      <main>
+        <div>
+          <Routes>
+            <Route path="/" exact element={<Tickets tickets={{}} />} />
+          </Routes>
+        </div>
+      </main>
+      <footer>
+        <div>Foxcon2022</div>
+      </footer>
+    </HashRouter>
+  )
+}
+
+export default Home
